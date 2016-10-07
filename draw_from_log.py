0bb2b2497f30a87900e18c3fccfe621cce46655a
jianyu <iam@jianyujianyu.com>
2016-10-07 14:03:17 +0800

    fixed problems about path

--- draw_from_log.py
+++ draw_from_log.py
@@ -7,7 +7,14 @@ from draw import *
 
 if __name__ == '__main__':
     file_name = sys.argv[1]  # out.log(.train)/out.log(.test)
-    pic_path = "./"  # .
+
+    if '/' in file_name:
+        pic_path = file_name[:file_name.rindex('/')+1]
+    else:
+        pic_path = "./"  # .
+
+    print("LOG FILE:" + file_name)
+    print("PIC GENERATING PATH:" + pic_path)
 
     train_dict_list, test_dict_list = parse_log(file_name)
 
